7fe3fc149dcf7486c2f113d077ba2d3881173dad
JordanMHebert <55060700+JordanMHebert@users.noreply.github.com>
2020-06-02 20:51:57 -0400

    update

--- contactform.php
+++ contactform.php
@@ -1,11 +1,16 @@
 <?php
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $phone = $_POST['phone'];
-  $message = $_POST['message'];
 
-  $email_from = 'jordanmhebert.com';
-  $email_subject = 'New Message from jordanmhebert.com Contact Form';
+$name=$email=$phone=$message="";
+$email_from=$email_subject=$email_body=$headers=$to="";
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+  $name = validateinput($_POST["name"]);
+  $email = validateinput($_POST["email"]);
+  $phone = validateinput($_POST["phone"]);
+  $message = validateinput($_POST["message"]);
+
+  $email_from = "jordanmhebert.com";
+  $email_subject = "New Message from jordanmhebert.com Contact Form";
   $email_body = "Name: $name.\n".
                 "Email: $email.\n".
                 "Phone: $phone.\n".
@@ -13,9 +18,17 @@
 
   $to ="jordan@jordanmhebert.com";
   $headers = "From: $email_from \r\n";
-  $headers .= "Reply-To: $email \r\n";
+  $headers = "Reply-To: $email \r\n";
 
   mail($to,$email_subject,$email_body,$headers);
 
-  header("location: index.html");
+  header("location: formsuccess.html");
+}
+
+  function validateinput($data){
+    $data=trim($data);
+    $data=stripslashes($data);
+    $data=htmlspecialchars($data);
+    return $data;
+  }
  ?>
